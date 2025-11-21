ecd266c835d497c76249f5497148b5116dfacceb
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-21 08:47:29 +0700

    membuat scaffold dan card

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -5,6 +5,28 @@ class CardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+              title: const Text("Dashboard"),
+              actions: const [],
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [
+                    Card(
+                      color: Colors.red,
+                      child: Text("Card with color",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
