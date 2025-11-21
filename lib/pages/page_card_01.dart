883e8ff9be77a999058ac697b496af46b55af678
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-21 09:46:56 +0700

    membuat column didalam card dan menambahkan jarak dengan sizedbox

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -6,27 +6,61 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              title: const Text("Dashboard"),
-              actions: const [],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [
-                    Card(
-                      color: Colors.red,
-                      child: Text("Card with color",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    )
-                  ],
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Card(
+                color: Colors.red,
+                child: Text(
+                  "Card with color",
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ),
-            ),
-          );
+              SizedBox(height: 20.0),
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.red,
+                ),
+                child: Text(
+                  "Container with color",
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Card(
+                elevation: 8,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Tentang Saya",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "ini adalah deskripsi tentang saya. saya seorang mahasiswa yang sedang belajar flutter di semester 5. saya sangat senang belajar flutter karena flutter sangat menyenangkan dan mudah untuk dipelajari.",
+                        style: TextStyle(fontSize: 12.0),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
