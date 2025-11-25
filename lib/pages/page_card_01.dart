1ebde41b4fa86e5acf51b67f6dc9fc7ca6fdb139
Mansyahariii <arifirmansyah350@gmail.com>
2025-11-25 16:14:53 +0700

    menambahkan text dan vertical divider pada page_card_01

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -57,6 +57,153 @@ class CardPage extends StatelessWidget {
                   ),
                 ),
               ),
+              Card(
+                elevation: 8,
+                shadowColor: Colors.red,
+                clipBehavior: Clip.antiAlias,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.red, Colors.orange],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 4.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.2),
+                              blurRadius: 10,
+                              offset: Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: CircleAvatar(
+                          radius: 60,
+                          backgroundImage: AssetImage(
+                            'assets/images/download.jpg',
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Mansyahari",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Text(
+                          "Flutter Developer",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "123",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Follower",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  // fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 30,
+                            child: VerticalDivider(
+                              thickness: 1.0,
+                              color: Colors.black,
+                              width: 15.0,
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "25 Year",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Experience",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  // fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 30,
+                            child: VerticalDivider(
+                              thickness: 1.0,
+                              color: Colors.black,
+                              width: 15.0,
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "234",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Projects",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  // fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
